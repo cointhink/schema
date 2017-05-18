aad98ba6562ff81617349b6d8cdd583179bebda2
Don Park <donp@donp.org>
2017-05-18 11:11:04 -0700

    created_at

--- migrations/20170517_create_accounts.sql
+++ migrations/20170517_create_accounts.sql
@@ -1,6 +1,7 @@
 -- +goose Up
 create table accounts (
   id varchar(128) primary key,
+  created_at timestamp default (now() at time zone 'utc'),
   fullname text,
   email text not null unique,
   username text
