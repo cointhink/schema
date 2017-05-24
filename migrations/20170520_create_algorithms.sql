7334331c69cdf0686a5a8ff503ae12045d8d7209
Don Park <donp@donp.org>
2017-05-24 09:37:17 -0700

    not a reference

--- migrations/20170520_create_algorithms.sql
+++ migrations/20170520_create_algorithms.sql
@@ -3,7 +3,7 @@ create table algorithms (
   id varchar(128) primary key,
   created_at timestamp default now(),
   account_id varchar(128) references accounts ,
-  status varchar(128) references accounts,
+  status varchar(128),
   language varchar(128),
   code text
 );
