3dceccbf1a67cbd69ebe13e758634a45730f8d47
Don Park <donp@donp.org>
2017-06-30 11:17:02 -0700

    name fix

--- migrations/20170630_alter_algoevents_to_algologs.sql
+++ migrations/20170630_alter_algoevents_to_algologs.sql
@@ -1,4 +1,6 @@
 -- +goose Up
 alter table algoevents rename to algologs;
+alter table algologs add event varchar(128);
 -- +goose Down
+alter table algologs drop event;
 alter table algologs rename to algoevents;
