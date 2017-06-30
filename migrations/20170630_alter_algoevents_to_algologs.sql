962d0f3a65249ed9a3040625b383404d0460da67
Don Park <donp@donp.org>
2017-06-30 11:22:21 -0700

    level/type fix

--- migrations/20170630_alter_algoevents_to_algologs.sql
+++ migrations/20170630_alter_algoevents_to_algologs.sql
@@ -1,6 +1,8 @@
 -- +goose Up
 alter table algoevents rename to algologs;
 alter table algologs add event varchar(128);
+alter table algologs rename type to level;
 -- +goose Down
+alter table algologs rename level to type;
 alter table algologs drop event;
 alter table algologs rename to algoevents;
