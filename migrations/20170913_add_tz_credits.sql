aa9cd5b543515026288d52f48b8d91584c91e4c8
Don Park <donp@donp.org>
2017-09-14 12:27:57 -0700

    you get two credits, and you get two credits

--- migrations/20170913_add_tz_credits.sql
+++ migrations/20170913_add_tz_credits.sql
@@ -2,7 +2,7 @@
 alter table accounts add time_zone text;
 update accounts set time_zone='';
 alter table accounts add schedule_credits integer;
-update accounts set schedule_credits = 1;
+update accounts set schedule_credits = 2;
 -- +goose Down
 alter table accounts drop schedule_credits;
 alter table accounts drop time_zone;
--- migrations/20170914_create_credit_journals.sql
+++ migrations/20170914_create_credit_journals.sql
@@ -9,5 +9,7 @@ create table credit_journals (
   credit_adjustment int,
   total_usd numeric(8,2)
 );
+insert into credit_journals (id, created_at, account_id, credit_adjustment, total_usd)
+select concat('credit_journals-',substr(md5(random()::text), 0, 10)), now(), id, 2, 0 from accounts;
 -- +goose Down
 drop table credit_journals;
